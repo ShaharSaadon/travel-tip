6440de5eb0f0e9ae654eb6e650cab8ba443cad15
ofekdov <ofekofek2002@gmail.com>
2023-02-22 17:29:45 +0200

    change

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,13 +2,32 @@ import {storageService} from './async-storage.service.js'
 import {mapService} from './map.service.js'
 export const locService = {
     getLocs,
-    createLoc,
     save,
 }
 
 const LOCATION_KEY = 'locationsDB'
 
-const locs = query()  
+const locs = [
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+]
+
+
+function query() {
+    // return axios.get()
+    return storageService.query(LOCATION_KEY)
+        .then(locations => {
+            // if (gFilterBy.txt) {
+            //     const regex = new RegExp(gFilterBy.txt, 'i')
+            //     pets = pets.filter(pet => regex.test(pet.name))
+            // }
+            // if (gFilterBy.minScore) {
+            //     pets = pets.filter(pet => pet.score >= gFilterBy.minScore)
+            // }
+            return pets
+        })
+}
+
 
 
 function getLocs() {
@@ -24,58 +43,3 @@ function save(loc) {
         return storageService.post(LOCATION_KEY, loc)
 }
 
-function query() {
-    // return axios.get()
-    return storageService.query(LOCATION_KEY)
-        .then(locs => {
-            // if (gFilterBy.txt) {
-            //     const regex = new RegExp(gFilterBy.txt, 'i')
-            //     pets = pets.filter(pet => regex.test(pet.name))
-            // }
-            // if (gFilterBy.minScore) {
-            //     pets = pets.filter(pet => pet.score >= gFilterBy.minScore)
-            // }
-            return locs
-        })
-}
-
-function get(locId) {
-    return storageService.get(LOCATION_KEY, locId)
-}
-
-function remove(locId) {
-    return storageService.remove(LOCATION_KEY, locId)
-    // return axios.remove(url, petId)
-}
-
-function createLoc(id,title,location){
-   let loc = {
-        id,
-        title,
-        location,
-        createdAt: Date.now(),
-        updateAt: Date.now(),
-        
-    } 
-     return Promise.resolve(loc)
-}
-
-// function _createPets() {
-//     let pets = utilService.loadFromStorage(PET_KEY)
-//     if (!pets || !pets.length) {
-//         _createDemoPets()
-//     }
-// }
-
-function _createDemoPets() {
-    const petNames = ['Bobi', 'Charli', 'Pinchi']
-    const petDescs = ['Bobi is an amazing dog', 'Charli is a curious cat', 'Just one look at Pinchi']
-
-    const pets = petNames.map((petName, i) => {
-        const pet = _createPet(petName)
-        pet.desc = petDescs[i]
-        return pet
-    })
-
-    utilService.saveToStorage(PET_KEY, pets)
-}
